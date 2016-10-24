7d1edfcc639c7b6a33483727d038ddd13f1a5a16
Ziqiang Lv <lvziqiang@smartconn.cc>
2016-10-24 23:12:53 +0800

    router base
    Change-Id: Ieb73bd47ab79703fc07086792e40ca9d7ce09bed

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+/* eslint-disable no-new */
+
+new Vue({
+  router,
+  el: '#app',
+  // template: '<App/>',
+  components: { App }
+})
